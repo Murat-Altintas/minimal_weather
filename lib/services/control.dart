f9f316f9808e6a1b99c06fc07b5808dc75d5cd5b
Murat-Altintas <murat.altintas.34@gmail.com>
2022-05-28 20:14:20 +0300

    "ver 1.5 sonderman"

--- lib/services/control.dart
+++ lib/services/control.dart
@@ -32,110 +32,60 @@ class ApiListFill {
   }
 
   void imageChangeVoid() {
-    List<int> cloudy = [1006, 1009];
-    List<int> lightRainy = [1063, 1150, 1153, 1183, 1198, 1240];
-    List<int> mediumRainy = [1066, 1072, 1087, 1168, 1186, 1189, 1192, 1243];
-    List<int> heavyRainy = [1171, 1195, 1201, 1273, 1276];
-    List<int> fog = [1030, 1135, 1147];
-    List<int> lightSnowy = [1066, 1069, 1024, 1210, 1213, 1216, 1249, 1255, 1261];
-    List<int> heavySnowy = [1114, 1117, 1207, 1219, 1222, 1225, 1237, 1258, 1264];
+    Map<String, List<int>> imageListMap = {
+      "cloudy": [1006, 1009],
+      "lightRainy": [1063, 1150, 1153, 1183, 1198, 1240],
+      "sunny": [1000],
+      "lightcloudy": [1003],
+      "mediumRainy": [1066, 1072, 1087, 1168, 1186, 1189, 1192, 1243],
+      "heavyRainy": [1171, 1195, 1201, 1273, 1276],
+      "fog": [1030, 1135, 1147],
+      "lightSnowy": [1066, 1069, 1024, 1210, 1213, 1216, 1249, 1255, 1261],
+      "heavySnowy": [1114, 1117, 1207, 1219, 1222, 1225, 1237, 1258, 1264]
+    };
 
-    if (imageCode == 1000) {
-      imageTop = "assets/lotties/4804-weather-sunny.json";
+    imageListMap.forEach((key, value) {
+      if (value.contains(imageCode)) imageTop = matchAssets(key);
+    });
+    for (int hil in hourlyImageList) {
+      imageListMap.forEach((String key, value) {
+        if (value.contains(hil)) {
+          hourlyyyy.add(matchAssets(key));
+        }
+      });
     }
+  }
 
-    for (var element in hourlyImageList) {
-      if (1000 == element) {
-        hourlyImage = "assets/lotties/4804-weather-sunny.json";
-        hourlyyyy.add(hourlyImage);
-      }
-    }
+  String matchAssets(String key) {
+    switch (key) {
+      case "cloudy":
+        return "assets/lotties/4806-weather-windy.json";
+      case "lightRainy":
+        return "assets/lotties/4801-weather-partly-shower.json";
 
-    if (imageCode == 1003) {
-      imageTop = "assets/lotties/4800-weather-partly-cloudy.json";
-    }
+      case "sunny":
+        return "assets/lotties/4804-weather-sunny.json";
 
-    for (var element in hourlyImageList) {
-      if (1003 == element) {
-        hourlyImage = "assets/lotties/4800-weather-partly-cloudy.json";
-        hourlyyyy.add(hourlyImage);
-      }
-    }
+      case "lightcloudy":
+        return "assets/lotties/4800-weather-partly-cloudy.json";
 
-    if (cloudy.contains(imageCode)) {
-      imageTop = "assets/lotties/4806-weather-windy.json";
-    }
+      case "mediumRainy":
+        return "assets/lotties/4805-weather-thunder.json";
 
-    for (var element in hourlyImageList) {
-      if (cloudy.contains(element)) {
-        hourlyImage = "assets/lotties/4806-weather-windy.json";
-        hourlyyyy.add(hourlyImage);
-      }
-    }
+      case "heavyRainy":
+        return "assets/lotties/4803-weather-storm.json";
 
-    if (fog.contains(imageCode)) {
-      imageTop = "assets/lotties/4795-weather-mist.json";
-    }
+      case "fog":
+        return "assets/lotties/4795-weather-mist.json";
 
-    for (var element in hourlyImageList) {
-      if (fog.contains(element)) {
-        hourlyImage = "assets/lotties/4795-weather-mist.json";
-        hourlyyyy.add(hourlyImage);
-      }
-    }
+      case "lightSnowy":
+        return "assets/lotties/4802-weather-snow-sunny.json";
 
-    if (mediumRainy.contains(imageCode)) {
-      imageTop = "assets/lotties/4805-weather-thunder.json";
-    }
+      case "heavySnowy":
+        return "assets/lotties/4793-weather-snow.json";
 
-    for (var element in hourlyImageList) {
-      if (mediumRainy.contains(element)) {
-        hourlyImage = "assets/lotties/4805-weather-thunder.json";
-        hourlyyyy.add(hourlyImage);
-      }
-    }
-
-    if (heavyRainy.contains(imageCode)) {
-      imageTop = "assets/lotties/4803-weather-storm.json";
-    }
-    for (var element in hourlyImageList) {
-      if (heavyRainy.contains(element)) {
-        hourlyImage = "assets/lotties/4803-weather-storm.json";
-        hourlyyyy.add(hourlyImage);
-      }
-    }
-
-    if (lightRainy.contains(imageCode)) {
-      imageTop = "assets/lotties/4801-weather-partly-shower.json";
-    }
-
-    for (var element in hourlyImageList) {
-      if (lightRainy.contains(element)) {
-        hourlyImage = "assets/lotties/4801-weather-partly-shower.json";
-        hourlyyyy.add(hourlyImage);
-      }
-    }
-
-    if (lightSnowy.contains(imageCode)) {
-      imageTop = "assets/lotties/4802-weather-snow-sunny.json";
-    }
-
-    for (var element in hourlyImageList) {
-      if (lightSnowy.contains(element)) {
-        hourlyImage = "assets/lotties/4802-weather-snow-sunny.json";
-        hourlyyyy.add(hourlyImage);
-      }
-    }
-
-    if (heavySnowy.contains(imageCode)) {
-      imageTop = "assets/lotties/4793-weather-snow.json";
-    }
-
-    for (var element in hourlyImageList) {
-      if (heavySnowy.contains(element)) {
-        hourlyImage = "assets/lotties/4793-weather-snow.json";
-        hourlyyyy.add(hourlyImage);
-      }
+      default:
+        return "";
     }
   }
 }
